ceb5dac3d5280fcf0166f5c887501fba1d6e440c
Johannes Koester <johannes.koester@uni-due.de>
2023-11-20 14:04:00 +0100

    fix import

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,7 @@
 from typing import Optional
 import snakemake.common.tests
 from snakemake_executor_plugin_drmaa import ExecutorSettings
-from snakemake_interface_executor_plugins import ExecutorSettingsBase
+from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase
 
 
 class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsLocalStorageBase):
